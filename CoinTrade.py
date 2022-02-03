3cb9359fc25a580569275f526df0b3908b43939d
louis71005 <96876130+louis71005@users.noreply.github.com>
2022-02-03 12:12:57 +0900

    Update CoinTrade.py

--- CoinTrade.py
+++ CoinTrade.py
@@ -148,18 +148,24 @@ while True:
                         krw = get_balance("KRW")
                         if krw > 5000:
                             upbit.buy_market_order(fsticker, krw*0.9995)
+                    else:
+                        cnt = 0
 
                 elif 2000 < target_price < 5000:
                     if current_price / target_price <= 1.005:
                         krw = get_balance("KRW")
                         if krw > 5000:
                             upbit.buy_market_order(fsticker, krw*0.9995)
+                    else:
+                        cnt = 0
                             
                 else:
                     if current_price / target_price <= 1.01:
                         krw = get_balance("KRW")
                         if krw > 5000:
                             upbit.buy_market_order(fsticker, krw*0.9995)
+                    else:
+                        cnt = 0
                             
 
             # 3% 이익일 경우 전량 매도
